5d1cab449588dfa38720f89ba2da3398be24086c
Ralf Schandl <rakus@users.noreply.github.com>
2019-11-22 09:39:09 +0100

    Prefer xsltproc to xpath, refactored parsing

--- .maven-completion.bash
+++ .maven-completion.bash
@@ -1,39 +1,17 @@
 #
 # Bash completion for MAVEN
 #
-# This was initally based on the maven completion Juven Xu.
-# See https://github.com/juven/maven-bash-completion
-#
-# PROFILES
-# --------
-# Profiles a dynamically parsed from the poms participating in the build. This
-# means all available parent poms and the child poms are parsed. Note that a
-# parent pom that is accessed via repository (local or remote) is not parsed.
-# Also the profiles from ~/.m2/settings.xml are parsed.
-#
-# The result of parsing is cached in environmnet variables:
-#   - __mvn_last_pom          holds the fq name of parsed pom
-#   - __mvn_last_pom_profiles holds profile names
-#
-# The cached values can be deleted using the command 'mvn_comp_reset'.
-#
-# PLUGINS:
-# The completion script loads additional completion-plugins from
-# ~/.maven-completion.d if available. The completions-plugins are
-# createable using the script `mvn-comp-create-plugin.sh`.
-# The registry for completion-plugins is cleared with the command
-# `mvn_comp_reset` and reloaded on the next invocation.
-#
-# RECOMMENDED: xpath or xsltproc
+# RECOMMENDED: xsltproc or xpath
 #
 # AUTHOR: Ralf Schandl
+# Based on work by Juven Xu. See https://github.com/juven/maven-bash-completion
 #
 # ----------------------------------------------------------------------------
 # shellcheck shell=bash
 # shellcheck disable=SC2207,SC2155
 
-_mvn_has_xpath="$(type -P xpath 2>/dev/null)"
 _mvn_has_xsltproc="$(type -P xsltproc 2>/dev/null)"
+_mvn_has_xpath="$(type -P xpath 2>/dev/null)"
 
 #mvc_debug()
 #{
@@ -96,35 +74,7 @@ unset -f _mvn_function_exists
 
 #---------[ POM parsing functions ]--------------------------------------------
 # Either using xpath or xslt or (as fallback) grep etc
-if [ -n "$_mvn_has_xpath" ]; then
-
-    __mvn_get_module_poms()
-    {
-        xpath -q -e '//modules/module/text()' "$1" | sed 's/$/\/pom.xml/'
-    }
-
-__mvn_get_pom_profiles()
-{
-    xpath -q -e '/project/profiles/profile/id/text()' "$1"
-}
-
-__mvn_get_parent_pom_path()
-{
-    local parent
-    if [ -n "$(xpath -q -e '/project/parent' "$1" &>/dev/null)" ]; then
-        parent=$(xpath -q -e '/project/parent/relativePath/text()' "$1")
-        [ -z "$parent" ] && parent='../pom.xml'
-        echo "$parent"
-    else
-        return 0
-    fi
-}
-__mvn_get_settings_profiles()
-{
-    xpath -q -e '/settings/profiles/profile/id/text()' "$HOME/.m2/settings.xml"
-}
-
-elif [ -n "$_mvn_has_xsltproc" ]; then
+if [ -n "$_mvn_has_xsltproc" ]; then
 
     __mvn_get_module_poms()
     {
@@ -144,7 +94,7 @@ EOF
 
 __mvn_get_pom_profiles()
 {
-    xsltproc - "$1" << EOF
+    xsltproc - "$@" << EOF
 <xsl:stylesheet version="1.0"
     xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
     xmlns:m="http://maven.apache.org/POM/4.0.0">
@@ -200,6 +150,34 @@ __mvn_get_settings_profiles()
 EOF
 }
 
+elif [ -n "$_mvn_has_xpath" ]; then
+
+    __mvn_get_module_poms()
+    {
+        xpath -q -e '//modules/module/text()' "$1" | sed 's/$/\/pom.xml/'
+    }
+
+__mvn_get_pom_profiles()
+{
+    xpath -q -e '/project/profiles/profile/id/text()' "$@"
+}
+
+__mvn_get_parent_pom_path()
+{
+    local parent
+    if [ -n "$(xpath -q -e '/project/parent' "$1" 2>/dev/null)" ]; then
+        parent=$(xpath -q -e '/project/parent/relativePath/text()' "$1")
+        [ -z "$parent" ] && parent='../pom.xml'
+        echo "$parent"
+    else
+        return 0
+    fi
+}
+__mvn_get_settings_profiles()
+{
+    xpath -q -e '/settings/profiles/profile/id/text()' "$HOME/.m2/settings.xml"
+}
+
 else
     #
     # Fallback using grep and friends. This is "best effort".
@@ -208,7 +186,7 @@ else
     __mvn_clean_pom()
     {
         # remove comments
-        sed 's/<!--.*-->//g;/<!--/,/-->/d' "$1"
+        sed 's/<!--.*-->//g;/<!--/,/-->/d' "$@"
     }
 
 __mvn_get_module_poms()
@@ -218,7 +196,7 @@ __mvn_get_module_poms()
 
 __mvn_get_pom_profiles()
 {
-    __mvn_clean_pom "$1" | grep -A2 '<profile>' | grep '<id>' | sed 's/<[^>]*>//g' | tr -d ' \t'
+    __mvn_clean_pom "$@" | grep -A2 '<profile>' | grep '<id>' | sed 's/<[^>]*>//g' | tr -d ' \t'
 }
 
 __mvn_get_parent_pom_path()
@@ -245,51 +223,46 @@ unset _mvn_has_xsltproc
 
 #---------[ Not parsing functions ]--------------------------------------------
 
-__mvn_get_module_poms_recursive()
+__mvn_get_poms_recursive()
 {
     echo "$1"
     local modules=$( __mvn_get_module_poms "$1" | sed "s%^%$(dirname "$1")/%")
     #mvc_debug "recModules: >>$modules<<"
     echo "$modules"
     for m in $modules; do
-        __mvn_get_module_poms_recursive "$m"
+        __mvn_get_poms_recursive "$m"
     done
 }
 
-__mvn_get_parent_profiles()
+__mvn_get_parent_poms()
 {
-    #mvc_debug "get_parent_profiles: $1"
+    local parent
     [ ! -e "$1" ] && return
-
-    local pom=$(__mvn_get_parent_pom_path "$1")
-    if [ -z "$pom" ]; then
-        # pom="../pom.xml"
+    local parent=$(__mvn_get_parent_pom_path "$1")
+    if [ -z "$parent" ]; then
         return
     fi
-    if [ ! -e "$pom" ]; then
-        #mvc_debug "does not exist: $pom"
+    parent="$(dirname "$1")/$parent"
+    if [ ! -e "$parent" ]; then
+        #mvc_debug "does not exist: $parent"
         return
     fi
-    local profs=$( __mvn_get_pom_profiles "$pom" )
-
-    profs=$( (echo "$profs"; __mvn_get_parent_profiles "$pom") | sort -u)
-    echo "$profs"
+    __mvn_get_parent_poms "$parent"
+    echo "$parent"
 }
 
 __mvn_get_profiles()
 {
-    local modules=$(__mvn_get_module_poms_recursive "$1" | sort -u)
-    #mvc_debug "modules: $modules"
-    for p in $modules; do
-        #mvc_debug "module: $p"
-        #get_pom_profiles "$p"
-        profs=$( (echo "$profs"; __mvn_get_pom_profiles "$p") | sort -u)
-        #mvc_debug "profiles: $profs"
-
-    done
-    profs=$( (echo "$profs"; __mvn_get_parent_profiles "$1") | sort -u)
-    local profiles=$(echo "$profs"|tr ' ' '|')
-    echo "$profiles"
+    if [ -z "$__mvn_last_pom_profiles" ]; then
+        local IFS profs modules
+        IFS=$'\n' modules=( $(__mvn_get_poms_recursive "$1" | sort -u) )
+        IFS=$'\n' parents=( $(__mvn_get_parent_poms "$1" | sort -u) )
+        #mvc_debug "modules: $modules"
+        IFS=$'\n' profs=( $(__mvn_get_pom_profiles "${modules[@]}" "${parents[@]}" | sort -u) )
+        IFS=$'\n' profs+=( $(__mvn_get_settings_profiles | sort -u) )
+        IFS='|' __mvn_last_pom_profiles="${profs[*]}"
+    fi
+    echo "$__mvn_last_pom_profiles"
 }
 
 
@@ -307,14 +280,12 @@ __mvn_filter_array()
 
 __mvn_plugin_goal()
 {
-    typeset -A plugin_goals
-
     local plugin goals suffix
 
     if [[ ${cur} == *:* ]] ; then
         local plugin="${cur%:*}"
         if [ -n "${__mvn_comp_plugins["$plugin"]}" ]; then
-            local goals="$($HOME/.maven-completion.d/${__mvn_comp_plugins["$plugin"]} goals | sed "s/^/$plugin:/;s/|/|$plugin:/g")"
+            local goals="$("$HOME/.maven-completion.d/${__mvn_comp_plugins[$plugin]}" goals | sed "s/^/$plugin:/;s/|/|$plugin:/g")"
             suffix=' '
         else
             local goals="$(__mvn_filter_array "$cur" "${!__mvn_comp_plugins[@]}")"
@@ -335,11 +306,11 @@ __mvn_init()
     unset __mvn_comp_plugins
     typeset -gA __mvn_comp_plugins
 
-    if [ "$(ls -tr $HOME/.maven-completion.d 2>/dev/null| tail -n1)" != "mc-plugin.cache" ]; then
+    # shellcheck disable=SC2012
+    if [ "$(ls -tr "$HOME/.maven-completion.d" 2>/dev/null| tail -n1)" != "mc-plugin.cache" ]; then
         if [ -d "$HOME/.maven-completion.d" ]; then
-            > "$HOME/.maven-completion.d/mc-plugin.cache"
+            true > "$HOME/.maven-completion.d/mc-plugin.cache"
             for pi in "$HOME/.maven-completion.d/"*.mc-plugin; do
-                init="$($pi register)"
                 for al in $($pi register); do
                     #echo >&2 "Registering: >>$al<<"
                     echo "__mvn_comp_plugins[\"$al\"]=\"$(basename "$pi")\"" >> "$HOME/.maven-completion.d/mc-plugin.cache"
@@ -347,6 +318,7 @@ __mvn_init()
             done
         fi
     fi
+    # shellcheck disable=SC1090
     . "$HOME/.maven-completion.d/mc-plugin.cache"
     __mvn_inited="true"
 }
@@ -372,46 +344,46 @@ _mvn()
 
     local options="-Dmaven.test.skip=true|-DskipTests|-DskipITs|-Dmaven.surefire.debug|-DenableCiProfile|-Dpmd.skip=true|-Dcheckstyle.skip=true|-Dtycho.mode=maven|-Dmaven.javadoc.skip=true|-Dgwt.compiler.skip|-Drestart|-Dtest.project.only|-DfullDeployment=true"
 
-    local profile_settings=$(__mvn_get_settings_profiles | tr '\n' '|' )
+    #local profile_settings=$(__mvn_get_settings_profiles | tr '\n' '|' )
 
     fqPom=$(readlink -f pom.xml)
     if [ ! -e "$fqPom" ]; then
-        #echo >&2 ""
-        #echo >&2 "ERROR: No pom.xml -- Hit Ctrl-C"
-        #return
+        # reset cache
         unset __mvn_last_pom_profiles
         unset __mvn_last_pom
     elif [ "$fqPom" != "$__mvn_last_pom" ]; then
-        # parse profiles and cache them
-        __mvn_last_pom_profiles=$(__mvn_get_profiles pom.xml)
+        # reset cache
         __mvn_last_pom=$fqPom
+        unset __mvn_last_pom_profiles
     fi
 
-    local profiles="${profile_settings}|${__mvn_last_pom_profiles}"
+    #local profiles="${profile_settings}|${__mvn_last_pom_profiles}"
     local IFS=$'|\n'
 
     if [[ ${cur} == -D* ]] ; then
         local pl_options=''
         local prev_parts part pl gl part
 
-        IFS=" " read -r -a prev_parts <<< $COMP_LINE
+        IFS=" " read -r -a prev_parts <<< "$COMP_LINE"
         for part in "${prev_parts[@]}"; do
             local pl="${part%:*}"
             local gl="${part##*:}"
             if [ -n "${__mvn_comp_plugins[$pl]}" ]; then
-                pl_options="${pl_options}$($HOME/.maven-completion.d/${__mvn_comp_plugins["$pl"]} goalopts $gl)"
+                pl_options="${pl_options}$("$HOME/.maven-completion.d/${__mvn_comp_plugins[$pl]}" goalopts "$gl")"
             fi
         done
         COMPREPLY=( $(compgen -S ' ' -W "${options}${pl_options}" -- "${cur}") )
 
     elif [[ ${cur} == -P* ]] ; then
         cur=${cur:2}
+        local profiles=$(__mvn_get_profiles pom.xml)
         if [[ ${cur} == *,* ]] ; then
             COMPREPLY=( $(compgen -S ',' -W "${profiles}" -P "-P${cur%,*}," -- "${cur##*,}") )
         else
             COMPREPLY=( $(compgen -S ',' -W "${profiles}" -P "-P" -- "${cur}") )
         fi
     elif [[ ${prev} == -P || ${prev} == --activate-profiles ]] ; then
+        local profiles=$(__mvn_get_profiles pom.xml)
         if [[ ${cur} == *,* ]] ; then
             COMPREPLY=( $(compgen -S ',' -W "${profiles}" -P "${cur%,*}," -- "${cur##*,}") )
         else
